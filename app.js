efdf3a8c23e56296be84fe57513eabb9d504aa02
NK-Forge <dev@nkforge.com>
2026-05-12 10:34:01 -0500

    chore: initialize express api project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'E-Commerce API is running'
+  });
+});
+
+module.exports = app;
